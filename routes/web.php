a5dcd97e8424130fdcbaced7bd5f492fb245263d
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-11-27 15:46:35 +0700

    update route clean

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,7 @@
 
 use App\Http\Controllers\JurusanController;
 use App\Http\Livewire\MahasiswaForm;
-use App\Http\Livewire\PostForm;
 use Illuminate\Support\Facades\Route;
-
-Route::get('posts', [PostForm::class, 'index'])->middleware(['auth'])->name('posts');
-Route::get('posts/exportPDF', [PostForm::class, 'exportPDF'])->middleware(['auth']);
 
 Route::get('mahasiswa', [MahasiswaForm::class, 'index'])->middleware(['auth'])->name('mahasiswa');
 Route::get('mahasiswa/exportPDF', [MahasiswaForm::class, 'exportPDF'])->middleware(['auth']);
